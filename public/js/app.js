e492979b46de10d88d33e98ddadc5a88aee7fb30
Idris Akbar Adyusman <idrissmp32@gmail.com>
2020-12-21 13:10:50 +0700

    test

--- public/js/app.js
+++ public/js/app.js
@@ -127756,8 +127756,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! A:\Programming\Web-Root\PPS-CAT\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! A:\Programming\Web-Root\PPS-CAT\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\PPS-CAT\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\PPS-CAT\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
